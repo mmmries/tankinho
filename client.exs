6351d6efc308f6b6be7bdcc2e39ddcc24104a321
Michael Ries <michael@riesd.com>
2018-06-19 16:44:30 -0600

    use option parsing and allow selecting other bots

--- client.exs
+++ client.exs
@@ -1,24 +1,30 @@
-options = case System.argv() do
-            [server_addr, name] ->
-              [addr: Tankinho.parse_server_addr(server_addr), name: name]
-            [server_addr, name, port] ->
-              [addr: Tankinho.parse_server_addr(server_addr), name: name, port: String.to_integer(port)]
-            _ ->
-              IO.puts "Please provide the address of the server and a name for your bot"
-              IO.puts "\teg. mix run client.exs 192.168.0.5:5566 Rusty"
-              IO.puts ""
-              IO.puts "You can also optionally specify which port you want to bind to"
-              IO.puts "\teg. mix run client.exs 192.168.0.5:5566 Rusty 5577"
-              exit(:normal)
-          end
+{opts, _args, []} = OptionParser.parse(System.argv(), switches: [server: :string, port: :integer, name: :string, tank_file: :string, tank_name: :string])
 
-Code.require_file("my_tank.ex")
+if !Keyword.has_key?(opts, :server) || !Keyword.has_key?(opts, :name) do
+  IO.puts "Please provide the address of the server and a name for your bot"
+  IO.puts "\teg. mix run --server 192.168.0.5:5566 --name Jill"
+  IO.puts ""
+  IO.puts "Other Options"
+  IO.puts "\t--port PORT the port number you want your client to bind to (default 5567)"
+  IO.puts "\t--tank-file FILE the file that defines your tank behavior (default MyTank.ex)"
+  IO.puts "\t--tank-name Name the module name of your tank behavior (default the basename of your tank file)"
+  exit(:normal)
+end
+
+server_addr = opts |> Keyword.get(:server) |> Tankinho.parse_server_addr()
+name = opts |> Keyword.get(:name)
+port = opts |> Keyword.get(:port, 5567)
+tank_file = opts |> Keyword.get(:tank_file, "MyTank.ex")
+tank_name = opts |> Keyword.get(:tank_name, Path.basename(tank_file, ".ex"))
+tank_module = :"Elixir.#{tank_name}"
+
+Code.require_file(tank_file)
 
 {:ok, _} = Tankinho.Client.start_link(%{
-  server_addr: Keyword.get(options, :addr),
-  module: MyTank,
-  name: Keyword.get(options, :name),
-  port: Keyword.get(options, :port, 5567),
+  server_addr: server_addr,
+  module: tank_module,
+  name: name,
+  port: port,
 })
-IO.puts "Started #{Keyword.get(options, :name)}"
-Tankinho.LiveReload.watch_for_changes([__DIR__], [MyTank])
+IO.puts "Started #{name}:#{port}"
+Tankinho.LiveReload.watch_for_changes([__DIR__], [tank_module])
