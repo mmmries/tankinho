ca5f19719865cce5c8744159040731f127c674f7
Michael Ries <michael@riesd.com>
2018-06-19 09:49:03 -0600

    update Client to use extracted Protocol

--- lib/tankinho/client.ex
+++ lib/tankinho/client.ex
@@ -1,65 +1,21 @@
 defmodule Tankinho.Client do
   use GenServer
-  require Logger
+  alias Tankinho.Protocol
 
   def start_link(args), do: GenServer.start_link(__MODULE__, args)
 
-  def init(%{server_addr: addr, name: name, port: port}) do
+  def init(%{server_addr: addr, name: name, port: port, module: module}) do
     {:ok, udp} = :gen_udp.open(port, [:binary])
-    send self(), :register
-    {:ok, %{addr: addr, name: name, status: :registering, socket: udp}}
+    protocol = Protocol.init(name, module, addr, udp)
+    {:ok, protocol}
   end
 
-  def handle_info(:register, %{status: :registering, addr: {addr, port}}=state) do
-    :ok = :gen_udp.send(state.socket, addr, port, "REG #{state.name}")
-    Process.send_after(self(), :register, 2_000)
-    {:noreply, state}
+  def handle_info({:udp, _, _, _, packet}, protocol) do
+    protocol = Protocol.handle_packet(protocol, packet)
+    {:noreply, protocol}
   end
-  def handle_info(:register, state), do: {:noreply, state}
-
-  # REGD
-  # this is an ACK for our registration
-  def handle_info({:udp, _, _, _, "REGD"}, %{status: :registering}=state) do
-    Logger.info "Registered as #{state.name}"
-    state = %{state | status: :registered}
-    {:noreply, state}
-  end
-  def handle_info({:udp, _, _, _, "ALIVE?"}, state=%{addr: {addr, port}}) do
-    :ok = :gen_udp.send(state.socket, addr, port, "ALIVE")
-    {:noreply, state}
-  end
-  # START_GAME 1600x1024 60
-  # game_widthxgame_height tank_size
-  def handle_info({:udp, _, _, _, "START_GAME"<>_}, %{status: :registered, addr: {addr,port}}=state) do
-    Logger.info "Joining Game"
-    state = %{state | status: :playing}
-    {:noreply, state}
-  end
-  # STATUS {"energy":100,"gun_heading":356,"heading":356,"radar_heading":356,"time":0,"speed":0,"x":1627,"y":1627,"robots_scanned":[],"broadcasts":[]}
-  # a status update about the game, time to send in your move
-  def handle_info({:udp, _, _, _, "STATUS "<>json}, %{status: :playing, addr: {addr,port}}=state) do
-    update = Jason.decode!(json)
-    if update["robots_scanned"] != [] || update["broadcasts"] != [] do
-      IO.puts "SCANNED SOMETHING #{json}"
-    end
-    #if rem(update["time"], 60) == 0 do
-    #  :ok = :gen_udp.send(state.socket, addr, port, "BROADCAST The day will be mine Trebek!")
-    #end
-    :ok = :gen_udp.send(state.socket, addr, port, "SAY Hey man, you think you'd be interested in something like that?")
-    :ok = :gen_udp.send(state.socket, addr, port, "ACCELERATE 0.2")
-    :ok = :gen_udp.send(state.socket, addr, port, "TURN 1")
-    :ok = :gen_udp.send(state.socket, addr, port, "FIRE 0.4")
-    {:noreply, state}
-  end
-  # GAME_OVER
-  # the game is over
-  def handle_info({:udp, _, _, _, "GAME_OVER"}, %{status: :playing}=state) do
-    Logger.info "Game Over, Man!"
-    {:noreply, %{state | status: :registered}}
-  end
-
-  def handle_info(other, state) do
-    Logger.error "#{__MODULE__} unexpected message (status=#{state.status}) #{inspect other}"
-    {:noreply, state}
+  def handle_info(message, protocol) do
+    protocol = Tankinho.Protocol.handle_info(protocol, message)
+    {:noreply, protocol}
   end
 end
--- two_tanks.exs
+++ two_tanks.exs
@@ -1,10 +1,28 @@
+[server_addr] = System.argv()
+server_addr = Tankinho.parse_server_addr(server_addr)
+
+defmodule ShrinkingViolet do
+  alias Tankinho.Actions
+
+  def init(_game_settings) do
+    %{turn_rate: :rand.uniform(30) - 15}
+  end
+
+  def tick(_events, %{turn_rate: rate}=state) do
+    actions = %Actions{} |> Actions.turn(rate)
+    {actions, state}
+  end
+end
+
 {:ok, _} = Tankinho.Client.start_link(%{
-  server_addr: {{127,0,0,1}, 5566},
+  server_addr: server_addr,
+  module: ShrinkingViolet,
   name: "tank1",
   port: 5567,
 })
 {:ok, _} = Tankinho.Client.start_link(%{
-  server_addr: {{127,0,0,1}, 5566},
+  server_addr: server_addr,
+  module: ShrinkingViolet,
   name: "tank2",
   port: 5568,
 })
