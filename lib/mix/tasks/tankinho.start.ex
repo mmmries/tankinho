80554a1958ceb8ec803a2f43b559238d1ce5f99a
Brett Beatty <brettbeatty@gmail.com>
2018-08-24 20:38:27 -0600

    running mix format on lib/mix/tasks/tankinho.start.ex

--- lib/mix/tasks/tankinho.start.ex
+++ lib/mix/tasks/tankinho.start.ex
@@ -10,25 +10,28 @@ defmodule Mix.Tasks.Tankinho.Start do
     {opts, _args, []} = OptionParser.parse(args, switches: @switches)
     check_for_required_options!(opts)
     tank_module = :"Elixir.#{opts[:tank_module]}"
-    {:ok, _} = Tankinho.Client.start_link(%{
-      server_addr: Tankinho.parse_server_addr(opts[:server]),
-      module: tank_module,
-      name: opts[:tank_name],
-      port: 0
-    })
+
+    {:ok, _} =
+      Tankinho.Client.start_link(%{
+        server_addr: Tankinho.parse_server_addr(opts[:server]),
+        module: tank_module,
+        name: opts[:tank_name],
+        port: 0
+      })
+
     IO.puts("Started tank #{opts[:tank_name]}")
     Tankinho.LiveReload.watch_for_changes([__DIR__], [tank_module])
   end
 
   defp check_for_required_options!(opts) do
-    unless Enum.all?(@required_options, &(Keyword.has_key?(opts, &1))) do
+    unless Enum.all?(@required_options, &Keyword.has_key?(opts, &1)) do
       print_usage()
       exit(:normal)
     end
   end
 
   defp print_usage do
-    IO.puts """
+    IO.puts("""
     Usage
 
       mix tankinho.start --server 127.0.0.1:5566 --tank-name Tank48 --tank_module Tankinho.ExampleTanks.Spinner
@@ -38,6 +41,6 @@ defmodule Mix.Tasks.Tankinho.Start do
       --server <ip>:<port>   Host & port running rrobots server
       --tank-name <name>     A display name for the tank
       --tank-module <module> The module containing behavior for the tank
-    """
+    """)
   end
 end
