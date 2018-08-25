6627b9f0433e3d940d3bf4f7da2651e335c0a11c
Brett Beatty <brettbeatty@gmail.com>
2018-08-24 22:04:15 -0600

    fixing mix docs for tankinho.start

--- lib/mix/tasks/tankinho.start.ex
+++ lib/mix/tasks/tankinho.start.ex
@@ -1,6 +1,21 @@
 defmodule Mix.Tasks.Tankinho.Start do
-  @moduledoc false
   use Mix.Task
+
+  @shortdoc "Starts a tank"
+
+  @moduledoc """
+  Usage
+
+      mix tankinho.start --server 127.0.0.1:5566 --tank-name Tank48 --tank_module Tankinho.ExampleTanks.Spinner
+
+  Options
+
+      --server <ip>:<port>   Host & port running rrobots server
+
+      --tank-name <name>     A display name for the tank
+
+      --tank-module <module> The module containing behavior for the tank
+  """
 
   @required_options [:server, :tank_name, :tank_module]
   @switches [server: :string, tank_name: :string, tank_module: :string]
@@ -31,16 +46,6 @@ defmodule Mix.Tasks.Tankinho.Start do
   end
 
   defp print_usage do
-    IO.puts("""
-    Usage
-
-      mix tankinho.start --server 127.0.0.1:5566 --tank-name Tank48 --tank_module Tankinho.ExampleTanks.Spinner
-
-    Options
-
-      --server <ip>:<port>   Host & port running rrobots server
-      --tank-name <name>     A display name for the tank
-      --tank-module <module> The module containing behavior for the tank
-    """)
+    IO.puts(@moduledoc)
   end
 end
